a3dad1733a26dad631204bd2d6747201791aaaea
github username <jhernandez1692@gmail.com>
2021-02-11 17:41:59 -0500

    Removed a column for better implimentation.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_235006) do
+ActiveRecord::Schema.define(version: 2021_02_11_223858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +25,11 @@ ActiveRecord::Schema.define(version: 2021_02_10_235006) do
     t.string "title"
     t.string "image_url"
     t.string "studio"
-    t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_games_on_category_id"
   end
 
+  add_foreign_key "games", "categories"
 end
